2a724f7988c367778743b9f3e6b9da773fefe737
sunshan <735160133@qq.com>
2021-01-20 13:00:30 +0800

    add:面试题5：相关题目，A1,A2数组组合排序

--- chapter2/面试题5：替换空格.go
+++ chapter2/面试题5：替换空格.go
@@ -1,17 +1,29 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 /*
 题目：将字符串中的空格替换成%20
 因为golang中字符串不可修改长度，所以思路还是用新的字符串替换旧的字符串
+知识点：
+1.如何更快的替换字符串
+2.要注意异常数据，如空字符串
+3.golang字符串的特性，不能修改，定长，只能使用[]byte或rune来修改后强转
 */
 func main() {
-	testStr := "We are happy"
-	testStr = strings.ReplaceAll(testStr, " ", "%20") //1使用写好的函数
-	println(testStr)
-	testStr2 := replaceStr(testStr, " ", "%20") //2使用自己重写一遍的函数，思路和源码还有剑指offer上的一样
-	println(testStr2)
+	//testStr := "We are happy"
+	//testStr = strings.ReplaceAll(testStr, " ", "%20") //1使用写好的函数
+	//println(testStr)
+	//testStr2 := replaceStr(testStr, " ", "%20") //2使用自己重写一遍的函数，思路和源码还有剑指offer上的一样
+	//println(testStr2)
+
+	arrayA1 := []int{1, 4, 8, 10, 15}
+	arrayA2 := []int{2, 5, 7, 9, 13}
+	arrayA3 := appendArray(arrayA1, arrayA2)
+	fmt.Print(arrayA3)
 }
 
 /*
@@ -56,3 +68,34 @@ func replaceStr(changeStr, oldStr, newStr string) string {
 我们可以先遍历一次字符串，这样就能统计出字符串中空格的总数，并可以由此计算出替换之后的字符串的总长度。
 每替换一个空格，长度增加2，因此替换以后字符串的长度等于原来的长度加上2乘以空格数目。我们还是以前面的字符串"We are happy."为例，"We are happy."这个字符串的长度是14（包括结尾符号'\0'），里面有两个空格，因此替换之后字符串的长度是18。
 */
+
+//相关题目
+/*
+有两个排序的数组A1和A2，内存在A1的末尾有足够多的空余空间容纳A2。请实现一个函数，把A2中的所有数字插入到A1中并且所有的数字是排序的。
+因为使用数组没有通用性，所以改用切片
+*/
+func appendArray(arrayA1 []int, arrayA2 []int) []int {
+	//1.从后到前遍历A2，如果A1[i]<A2[j],将A2[j]加到A1后面，否则交换
+	//2.使用copy
+	a1Index := len(arrayA1) - 1
+
+	//如果A1容量不足，扩容到满足A1+A2
+	if cap(arrayA1) < len(arrayA1)+len(arrayA2) {
+		arrayA1 = append(arrayA1, arrayA2...)
+	}
+	//for i:= 0;i< len(arrayA2);i++{
+	//	arrayA1= append(arrayA1, 0)
+	//}
+	for i := len(arrayA2) - 1; i >= 0; i-- {
+
+		_ = copy(arrayA1[a1Index+1:], arrayA1[a1Index:]) //将内容整体复制到后一位
+		if arrayA1[a1Index] <= arrayA2[i] {
+			arrayA1[a1Index+1] = arrayA2[i] //A1原来位置后一个的数据给上去
+		} else {
+			arrayA1[a1Index] = arrayA2[i] //A1原来位置的数据给上去
+
+		}
+		a1Index--
+	}
+	return arrayA1
+}
